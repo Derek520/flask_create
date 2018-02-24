4dd74acfd94bdf908c7f566f9067f9e0498b0e71
derek_520 <18700862394@139.com>
2018-02-24 22:06:20 +0800

    config配置文件

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.main import api
-
+from config import config_dict
 
 # 创建Flask实例对象
 app = Flask(__name__)
-
+# 配置文件需要紧跟着app创建
+app.config.from_object(config_dict['develop'])
 # 创建数据库对象
 db = SQLAlchemy(app)
 
--- config.py
+++ config.py
@@ -1,1 +1,22 @@
 # -*- coding:utf-8 -*-
+# config.py实现配置信息
+
+class Config(object):
+    '''配置文件'''
+    # mysql数据库配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1/ihome21'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+  DEBUG = True
+
+class ProductionConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1/ihome21'
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    pass
+
+config_dict = {
+    'develop':DevelopmentConfig,
+    'production':ProductionConfig
+}
