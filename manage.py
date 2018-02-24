b1738829aceca5faa906c0b96e7f1683eb3ae56d
derek_520 <18700862394@139.com>
2018-02-24 20:56:37 +0800

    manage配置

--- manage.py
+++ manage.py
@@ -4,9 +4,16 @@
 from flask import Flask
 from flask_script import Manager
 from flask_migrate import MigrateCommand,Migrate
+from app import db
 
+# 创建Flask实例对象
 app = Flask(__name__)
-
+# 创建命令行Manager对象
+manager = Manager(app)
+# 创建数据库迁移对象
+Migrate(app,db)
+# 创建命令行数据库迁移指令
+manager.add_command('db',MigrateCommand)
 
 @app.route('/')
 def hello_world():
@@ -14,4 +21,6 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    # app.run(debug=True,host='0.0.0.0')
+    # 使用manger启动
+    manager.run()
